155b29c532a271a3a6f52a8f5166dc6699724c4e
matthew-heideman <heidemanmr@yahoo.com>
2020-04-15 18:15:15 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios';
+import NasaCardContainer from './components/NasaCardContainer.js'
+
 
 function App() {
+  useEffect(() => {
+    const apiKey = 'PuK5ryfBfKZ0Ebrg7Ue6BQSJyR2oLuBjyA8ZFW0e';
+    const url = 'https://api.nasa.gov'
+    axios
+      .get(`${url}/planetary/apod?api_key=${apiKey}`)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error, 'Data was not retrieved')
+      })
+  })
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      <NasaCardContainer />
     </div>
   );
 }
---
+++ src/components/NasaCardContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function NasaCardContainer(){
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default NasaCardContainer;
